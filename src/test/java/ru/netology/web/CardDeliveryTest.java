4edc8b1ef50c2470f7f3d64a5886a872ceb6027d
yashinaQA42 <presnyakovae@gmail.com>
2023-02-26 22:06:07 +0300

    add allure

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -1,6 +1,10 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -15,6 +19,13 @@ import static ru.netology.web.DataGenerator.Registration.generateDate;
 import static ru.netology.web.DataGenerator.Registration.generateUser;
 
 public class CardDeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }@AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");}
 
     @BeforeEach
     void SetUp() {
